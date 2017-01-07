0979a1a0513218d64625934419a01608413cb041
Johan <johan.olofsson@gmail.com>
2017-01-07 11:47:38 +0100

    download support

--- service-worker.js
+++ service-worker.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["index.html","24ec56f48918e7279c5921c8133a5643"],["manifest.json","85d848fc3973e16c5d8c195193ebecb1"],["static/css/main.b0c148b3.css","4bfe07d41e3ae8a5f5e1813228a1b8d9"],["static/js/main.50cf3040.js","0be01e5336b437c6b94ae49aa6676f79"],["static/media/logo.5d5d9eef.svg","5d5d9eefa31e5e13a6610d9fa7a283bb"]];
+var precacheConfig = [["index.html","9ccdb371e530c790a1e38fbb2a254f5f"],["manifest.json","85d848fc3973e16c5d8c195193ebecb1"],["static/css/main.8289d30d.css","214ebc59636eb54d5164bcd9bc674cad"],["static/js/main.32969138.js","147d63ae774ce20480ec84ecd9c62185"],["static/media/logo.5d5d9eef.svg","5d5d9eefa31e5e13a6610d9fa7a283bb"]];
 var cacheName = 'sw-precache-v2-sw-precache-' + (self.registration ? self.registration.scope : '');
 
 
